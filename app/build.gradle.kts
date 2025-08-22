9b2474d07f78e79e0bbec4f418e7705654fd0710
Anshu Kumar Singh <thisissidam@gmail.com>
2025-08-22 12:58:14 +0530

    feat: Enable minification for release builds and set debug signing config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,8 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
