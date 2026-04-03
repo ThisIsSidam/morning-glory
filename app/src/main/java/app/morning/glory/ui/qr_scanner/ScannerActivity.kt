f6275eef99fdc6db1cbd629c285f8ef145090acc
Anshu Kumar Singh <thisissidam@gmail.com>
2026-04-03 23:37:55 +0530

    feat: modified capture layout to add a torch option

--- app/src/main/java/app/morning/glory/ui/qr_scanner/ScannerActivity.kt
+++ app/src/main/java/app/morning/glory/ui/qr_scanner/ScannerActivity.kt
@@ -1,30 +1,70 @@
 package app.morning.glory.ui.qr_scanner
 
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.KeyEvent
 import android.view.View
+import android.widget.Button
 import app.morning.glory.R
 import com.journeyapps.barcodescanner.CaptureActivity
 import com.journeyapps.barcodescanner.CaptureManager
 import com.journeyapps.barcodescanner.DecoratedBarcodeView
 
-class ScannerActivity : CaptureActivity() {
+class ScannerActivity : CaptureActivity(), DecoratedBarcodeView.TorchListener {
 
-    private lateinit var  capture: CaptureManager
+    private lateinit var capture: CaptureManager
     private lateinit var barcodeScannerView: DecoratedBarcodeView
+    private lateinit var switchFlashlightButton: Button
+    private var isFlashlightOn = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         barcodeScannerView = initializeContent()
+        barcodeScannerView.setTorchListener(this)
 
         capture = CaptureManager(this, barcodeScannerView)
         capture.initializeFromIntent(intent, savedInstanceState)
         capture.decode()
+
+        switchFlashlightButton = findViewById(R.id.switch_flashlight)
+
+        // if the device does not have flashlight in its camera,
+        // then remove the switch flashlight button...
+        if (!hasFlash()) {
+            switchFlashlightButton.visibility = View.GONE
+        }
     }
 
-    override fun initializeContent() : DecoratedBarcodeView {
+    override fun initializeContent(): DecoratedBarcodeView {
         setContentView(R.layout.capture_layout)
-        return findViewById<View?>(R.id.zxing_barcode_scanner) as DecoratedBarcodeView
+        return findViewById<View>(R.id.zxing_barcode_scanner) as DecoratedBarcodeView
+    }
+
+    /**
+     * Toggle the flashlight
+     */
+    fun switchFlashlight(view: View) {
+        if (isFlashlightOn) {
+            barcodeScannerView.setTorchOff()
+        } else {
+            barcodeScannerView.setTorchOn()
+        }
+    }
+
+    private fun hasFlash(): Boolean {
+        return applicationContext.packageManager
+            .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH)
+    }
+
+    override fun onTorchOn() {
+        switchFlashlightButton.setText(R.string.turn_off_flashlight)
+        isFlashlightOn = true
+    }
+
+    override fun onTorchOff() {
+        switchFlashlightButton.setText(R.string.turn_on_flashlight)
+        isFlashlightOn = false
     }
 
     override fun onResume() {
